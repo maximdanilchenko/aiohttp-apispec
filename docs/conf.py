f7724f1e6d5e9e173eeaa620a341f0e55008ecd6
Максим Данильченко <m.danilchenko@atol.ru>
2018-08-24 11:26:34 +0300

    theeming

--- docs/conf.py
+++ docs/conf.py
@@ -13,12 +13,13 @@ version = '0.3.2'
 source_suffix = '.rst'
 master_doc = 'index'
 pygments_style = 'sphinx'
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 html_theme_options = {
     'description': 'Build and document REST APIs with aiohttp and apispec',
     'show_powered_by': False,
+    'display_version': True,
 }
 html_title = 'aiohttp-apispec Documentation'
 html_short_title = 'aiohttp-apispec'
