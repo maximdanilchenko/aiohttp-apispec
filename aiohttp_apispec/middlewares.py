a531a164f719f9d0077a6a3cd6730c3d6de6926c
Ivan Larin <ivan.larin@sola.foundation>
2018-10-17 13:44:27 +0300

    Proper middleware method handling for CBV

--- aiohttp_apispec/middlewares.py
+++ aiohttp_apispec/middlewares.py
@@ -23,7 +23,9 @@ async def validation_middleware(request: web.Request, handler) -> web.Response:
     if not hasattr(orig_handler, '__schemas__'):
         if not issubclass_py37fix(orig_handler, web.View):
             return await handler(request)
-        sub_handler = getattr(orig_handler, request.method.lower())
+        sub_handler = getattr(orig_handler, request.method.lower(), None)
+        if sub_handler is None:
+            return await handler(request)
         if not hasattr(sub_handler, '__schemas__'):
             return await handler(request)
         schemas = sub_handler.__schemas__
