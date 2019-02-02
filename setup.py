890e4085f51ded8de3acbc83d695dac25268c6b4
Максим Данильченко <m.danilchenko@atol.ru>
2019-02-02 21:17:38 +0300

    version update from 0.7.7 to 0.7.8

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme(fname):
 
 setup(
     name='aiohttp-apispec',
-    version='0.7.7',
+    version='0.7.8',
     description='Build and document REST APIs with aiohttp and apispec',
     long_description=readme('README.md'),
     long_description_content_type="text/markdown",
