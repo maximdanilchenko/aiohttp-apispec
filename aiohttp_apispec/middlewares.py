7c1ea433a0014b04e4e506d6495aac5c617dbe5f
Dave <pzhang65@uwo.ca>
2021-08-29 00:44:20 -0400

    switch locations arg to location and pass unknown=None to use schema defined unknown field behavior

--- aiohttp_apispec/middlewares.py
+++ aiohttp_apispec/middlewares.py
@@ -31,7 +31,10 @@ async def validation_middleware(request: web.Request, handler) -> web.Response:
     result = {}
     for schema in schemas:
         data = await request.app["_apispec_parser"].parse(
-            schema["schema"], request, locations=schema["locations"]
+            schema["schema"],
+            request,
+            location=schema["location"],
+            unknown=None # Pass None to use the schema’s setting instead.
         )
         if schema["put_into"]:
             request[schema["put_into"]] = data
